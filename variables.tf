bdb6f7e03e5dae362c5329f881780f918eb8aff9
devopseasylearning <info@devopseasylearning.com>
2024-02-21 15:37:55 -0600

    first push

---
+++ variables.tf
@@ -0,0 +1,44 @@
+# envirnment variables
+
+variable "aws_region" {
+
+}
+
+variable "project_name" {
+
+}
+
+variable "enviromnent" {
+
+}
+
+# vpc variables
+
+variable "vpc_cidr" {
+
+}
+
+variable "public_subnet_az1_cidr" {
+
+}
+
+variable "public_subnet_az2_cidr" {
+
+}
+
+variable "private_app_subnet_az1_cidr" {
+
+}
+
+
+variable "private_app_subnet_az2_cidr" {
+
+}
+
+variable "private_data_subnet_az1_cidr" {
+
+}
+
+variable "private_data_subnet_az2_cidr" {
+
+}
